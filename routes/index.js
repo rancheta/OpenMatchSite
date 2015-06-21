9dbd49c354faa3963af55d518bd144c95121f68d
FIlburtTurtle <rancheta@berkeley.edu>
2015-06-20 19:09:02 -0700

    first commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,41 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+/* GET home page. */
+router.get('/features', function(req, res) {
+  res.render('features');
+});
+router.get('/enterprise', function(req, res) {
+  res.render('enterprise');
+});
+router.get('/about', function(req, res) {
+  res.render('about');
+});
+router.get('/contact', function(req, res) {
+  res.render('contact');
+});
+router.get('/docs', function(req, res) {
+  res.render('docs');
+});
+router.get('/docs/getting-started', function(req, res) {
+  res.render('./docs/getting-started');
+});
+router.get('/docs/configs', function(req, res) {
+  res.render('docs/configs');
+});
+router.get('/docs/dependencies', function(req, res) {
+  res.render('docs/dependencies');
+});
+router.get('/docs/features-in-development', function(req, res) {
+  res.render('docs/features-in-development');
+});
+router.get('/docs/file-hierarchy', function(req, res) {
+  res.render('docs/file-hierarchy');
+});
+router.get('/docs/tests', function(req, res) {
+  res.render('docs/tests');
 });
 
 module.exports = router;

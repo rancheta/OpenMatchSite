c6f3a09cf2cee403d0e4565b1b651af00d8ae0ec
FIlburtTurtle <rancheta@berkeley.edu>
2015-06-22 15:45:35 -0700

    404 page

--- openmatchwebsite.js
+++ openmatchwebsite.js
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
+    res.status(404).render('404');
     next(err);
 });
 
